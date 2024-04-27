ddcef7d9e160eb1e305eb3eb88f301a314681c69
Trishna <trishna.patil13@gmail.com>
2024-04-26 20:54:51 -0400

    latest changes

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * File for setting up the environment. Please do not modify.
+ */
+
+import "./css/readonly.css";
+import "./css/style.css";
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+console.log("DOM Loaded");
